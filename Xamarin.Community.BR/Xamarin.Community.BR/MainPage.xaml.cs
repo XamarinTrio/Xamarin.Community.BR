dcc394a1242f949a198c79de017129dcf3a7e00a
Felipe Baltazar <felipe.dasilvabaltazar@gmail.com>
2020-09-01 22:16:35 -0300

    ✨Incluso controle xamarino

--- Xamarin.Community.BR/Xamarin.Community.BR/MainPage.xaml.cs
+++ Xamarin.Community.BR/Xamarin.Community.BR/MainPage.xaml.cs
@@ -1,21 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
 using Xamarin.Forms;
 
 namespace Xamarin.Community.BR
 {
-    // Learn more about making custom code visible in the Xamarin.Forms previewer
-    // by visiting https://aka.ms/xamarinforms-previewer
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            xamarino.IniciarAnimacao();
         }
     }
 }
