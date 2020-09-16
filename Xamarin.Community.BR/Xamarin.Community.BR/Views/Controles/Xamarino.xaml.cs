30c8fda8513c326fdb1a44467ad36c1b28a7a780
Felipe Baltazar <felipe.dasilvabaltazar@gmail.com>
2020-09-15 22:11:30 -0300

    ♻ Refatoração Xamarino

--- Xamarin.Community.BR/Xamarin.Community.BR/Views/Controles/Xamarino.xaml.cs
+++ Xamarin.Community.BR/Xamarin.Community.BR/Views/Controles/Xamarino.xaml.cs
@@ -1,8 +1,5 @@
-﻿
-using System;
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 using Xamarin.Forms.Shapes;
-using Xamarin.Forms.Xaml;
 
 namespace Xamarin.Community.BR.Views.Controles
 {
@@ -10,7 +7,7 @@ namespace Xamarin.Community.BR.Views.Controles
     {
         private readonly PathGeometryConverter _conversor = new PathGeometryConverter();
 
-        public static BindableProperty PosicaoProperty = BindableProperty.Create(
+        public static readonly BindableProperty PosicaoProperty = BindableProperty.Create(
             nameof(Posicao),
             typeof(XamarinoPosicao),
             typeof(Xamarino),
@@ -18,124 +15,21 @@ namespace Xamarin.Community.BR.Views.Controles
             BindingMode.OneWay,
             propertyChanged: AlterarPosicao);
 
-        public static BindableProperty OlhoDireitoAbertoProperty = BindableProperty.Create(
-            nameof(OlhoDireitoAberto),
-            typeof(string),
+        private static readonly BindableProperty EscalaSVGProperty = BindableProperty.Create(
+            nameof(EscalaSVG),
+            typeof(double),
             typeof(Xamarino),
-            SVG.XAMARINO_OLHO_DIREITO_ABERTO,
-            BindingMode.OneWay);
+            1.0,
+            propertyChanged: AlterarEscalaSVG);
 
-        public static BindableProperty OlhoDireitoPiscandoProperty = BindableProperty.Create(
-            nameof(OlhoDireitoPiscando),
-            typeof(string),
-            typeof(Xamarino),
-            SVG.XAMARINO_OLHO_DIREITO_PISCANDO,
-            BindingMode.OneWay);
+        private OlhosFrames olhos = OlhosFrames.Frente();
 
-        public static BindableProperty OlhoDireitoFechadoProperty = BindableProperty.Create(
-            nameof(OlhoDireitoFechado),
-            typeof(string),
-            typeof(Xamarino),
-            SVG.XAMARINO_OLHO_DIREITO_FECHADO,
-            BindingMode.OneWay);
-
-        public static BindableProperty OlhoEsquerdoAbertoProperty = BindableProperty.Create(
-            nameof(OlhoEsquerdoAberto),
-            typeof(string),
-            typeof(Xamarino),
-            SVG.XAMARINO_OLHO_ESQUERDO_ABERTO,
-            BindingMode.OneWay);
-
-        public static BindableProperty OlhoEsquerdoPiscandoProperty = BindableProperty.Create(
-            nameof(OlhoEsquerdoPiscando),
-            typeof(string),
-            typeof(Xamarino),
-            SVG.XAMARINO_OLHO_ESQUERDO_PISCANDO,
-            BindingMode.OneWay);
-
-        public static BindableProperty OlhoEsquerdoFechadoProperty = BindableProperty.Create(
-            nameof(OlhoEsquerdoFechado),
-            typeof(string),
-            typeof(Xamarino),
-            SVG.XAMARINO_OLHO_ESQUERDO_FECHADO,
-            BindingMode.OneWay);
-
-        private static void AlterarPosicao(BindableObject bindable, object valorAtual, object novoValor)
-        {
-            if(bindable is Xamarino xamarino)
-            {
-                var posicao = (XamarinoPosicao)novoValor;
-                var resultado = VisualStateManager.GoToState(xamarino, posicao.ToString());
-
-                switch (posicao) {
-                    case XamarinoPosicao.Frente:
-                        xamarino.OlhoDireitoAberto = SVG.XAMARINO_OLHO_DIREITO_ABERTO;
-                        xamarino.OlhoDireitoPiscando = SVG.XAMARINO_OLHO_DIREITO_PISCANDO;
-                        xamarino.OlhoDireitoFechado = SVG.XAMARINO_OLHO_DIREITO_FECHADO;
-
-                        xamarino.OlhoEsquerdoAberto = SVG.XAMARINO_OLHO_ESQUERDO_ABERTO;
-                        xamarino.OlhoEsquerdoPiscando = SVG.XAMARINO_OLHO_ESQUERDO_PISCANDO;
-                        xamarino.OlhoEsquerdoFechado = SVG.XAMARINO_OLHO_ESQUERDO_FECHADO;
-                        break;
-                    case XamarinoPosicao.Esquerda:
-                        xamarino.OlhoDireitoAberto =   SVG.XAMARINO_ESQUERDA_OLHO_DIREITO_ABERTO;
-                        xamarino.OlhoDireitoPiscando = SVG.XAMARINO_ESQUERDA_OLHO_DIREITO_PISCANDO;
-                        xamarino.OlhoDireitoFechado =  SVG.XAMARINO_ESQUERDA_OLHO_DIREITO_FECHADO;
-
-                        xamarino.OlhoEsquerdoAberto =   SVG.XAMARINO_ESQUERDA_OLHO_ESQUERDO_ABERTO;
-                        xamarino.OlhoEsquerdoPiscando = SVG.XAMARINO_ESQUERDA_OLHO_ESQUERDO_PISCANDO;
-                        xamarino.OlhoEsquerdoFechado =  SVG.XAMARINO_ESQUERDA_OLHO_ESQUERDO_FECHADO;
-                        break;
-                    case XamarinoPosicao.Direita:
-                        break;
-                    case XamarinoPosicao.Cima:
-                        break;
-                    case XamarinoPosicao.Baixo:
-                        break;
-                    default:
-                        break;
-                }
-            }
+        private double EscalaSVG {
+            get => (double)GetValue(EscalaSVGProperty);
+            set => SetValue(EscalaSVGProperty, value);
         }
 
-        public string OlhoDireitoAberto
-        {
-            get => (string)GetValue(OlhoDireitoAbertoProperty);
-            set => SetValue(OlhoDireitoAbertoProperty, value);
-        }
-
-        public string OlhoDireitoPiscando
-        {
-            get => (string)GetValue(OlhoDireitoPiscandoProperty);
-            set => SetValue(OlhoDireitoPiscandoProperty, value);
-        }
-
-        public string OlhoDireitoFechado
-        {
-            get => (string)GetValue(OlhoDireitoFechadoProperty);
-            set => SetValue(OlhoDireitoFechadoProperty, value);
-        }
-
-        public string OlhoEsquerdoAberto
-        {
-            get => (string)GetValue(OlhoEsquerdoAbertoProperty);
-            set => SetValue(OlhoEsquerdoAbertoProperty, value);
-        }
-
-        public string OlhoEsquerdoPiscando
-        {
-            get => (string)GetValue(OlhoEsquerdoPiscandoProperty);
-            set => SetValue(OlhoEsquerdoPiscandoProperty, value);
-        }
-
-        public string OlhoEsquerdoFechado
-        {
-            get => (string)GetValue(OlhoEsquerdoFechadoProperty);
-            set => SetValue(OlhoEsquerdoFechadoProperty, value);
-        }
-
-        public XamarinoPosicao Posicao 
-        { 
+        public XamarinoPosicao Posicao {
             get => (XamarinoPosicao)GetValue(PosicaoProperty);
             set => SetValue(PosicaoProperty, value);
         }
@@ -143,6 +37,8 @@ namespace Xamarin.Community.BR.Views.Controles
         public Xamarino()
         {
             InitializeComponent();
+            fundo.Data = (Geometry)_conversor.ConvertFromInvariantString(Constantes.SVG.Xamarino.FUNDO);
+            AtualizarOlhos(olhos.Direito.Aberto, olhos.Esquerdo.Aberto);
         }
 
         public void IniciarAnimacao()
@@ -151,16 +47,24 @@ namespace Xamarin.Community.BR.Views.Controles
             animacao.Commit(this, "Xamarino_Piscando", rate: 32, length: 4000, repeat: () => true);
         }
 
+        protected override void OnSizeAllocated(double width, double height)
+        {
+            base.OnSizeAllocated(width, height);
+
+            if (grid != null && grid.Height > 0)
+                EscalaSVG = (grid.Height / 116.1);
+        }
+
         private void XamarinoAnimacao(double frame)
         {
             if (frame > .99) {
-                AtualizarOlhos(OlhoDireitoFechado, OlhoEsquerdoFechado);
+                AtualizarOlhos(olhos.Direito.Fechado, olhos.Esquerdo.Fechado);
             }
             else if (frame > .95) {
-                AtualizarOlhos(OlhoDireitoPiscando, OlhoEsquerdoPiscando);
+                AtualizarOlhos(olhos.Direito.Piscando, olhos.Esquerdo.Piscando);
             }
             else {
-                AtualizarOlhos(OlhoDireitoAberto, OlhoEsquerdoAberto);
+                AtualizarOlhos(olhos.Direito.Aberto, olhos.Esquerdo.Aberto);
             }
         }
 
@@ -168,13 +72,150 @@ namespace Xamarin.Community.BR.Views.Controles
         {
             if (string.IsNullOrWhiteSpace(olhoEsquerdo) || string.IsNullOrWhiteSpace(olhoDireito))
                 return;
-
+            
+            var centroX = fundo.Width / 2;
             var geometria = (Geometry)_conversor.ConvertFromInvariantString(olhoDireito);
             olho_direito.Data = geometria;
+            olho_direito.RenderTransform = new ScaleTransform(EscalaSVG, EscalaSVG, centroX, 0);
 
             geometria = (Geometry)_conversor.ConvertFromInvariantString(olhoEsquerdo);
             olho_esquerdo.Data = geometria;
+            olho_esquerdo.RenderTransform = new ScaleTransform(EscalaSVG, EscalaSVG, centroX, 0);
         }
+
+        private static void AlterarEscalaSVG(BindableObject bindable, object valorAtual, object novoValor)
+        {
+            if (bindable is Xamarino xamarino) {
+                var novaEscala = (double)novoValor;
+                var fundo = xamarino.fundo;
+                var centroX = fundo.Width /2;
+
+                fundo.RenderTransform = new ScaleTransform(novaEscala, novaEscala, centroX, 0);
+            }
+        }
+
+        private static void AlterarPosicao(BindableObject bindable, object valorAtual, object novoValor)
+        {
+            if (bindable is Xamarino xamarino) {
+                var posicao = (XamarinoPosicao)novoValor;
+
+                switch (posicao) {
+                    case XamarinoPosicao.Esquerda:
+                        xamarino.olhos = OlhosFrames.Esquerda();
+                        break;
+                    case XamarinoPosicao.Direita:
+                        xamarino.olhos = OlhosFrames.Direita();
+                        break;
+                    case XamarinoPosicao.Cima:
+                        xamarino.olhos = OlhosFrames.Cima();
+                        break;
+                    case XamarinoPosicao.Baixo:
+                        xamarino.olhos = OlhosFrames.Baixo();
+                        break;
+                    case XamarinoPosicao.Frente:
+                    default:
+                        xamarino.olhos = OlhosFrames.Frente();
+                        break;
+                }
+            }
+        }
+    }
+
+    public struct OlhosFrames
+    {
+        public OlhoFrame Esquerdo { get; set; }
+
+        public OlhoFrame Direito { get; set; }
+
+        public static OlhosFrames Frente()
+        {
+            return new OlhosFrames() {
+                Direito = new OlhoFrame() {
+                    Aberto = Constantes.SVG.Xamarino.Frente.OLHO_DIREITO_ABERTO,
+                    Piscando = Constantes.SVG.Xamarino.Frente.OLHO_DIREITO_PISCANDO,
+                    Fechado = Constantes.SVG.Xamarino.Frente.OLHO_DIREITO_FECHADO
+                },
+                Esquerdo = new OlhoFrame() {
+                    Aberto = Constantes.SVG.Xamarino.Frente.OLHO_ESQUERDO_ABERTO,
+                    Piscando = Constantes.SVG.Xamarino.Frente.OLHO_ESQUERDO_PISCANDO,
+                    Fechado = Constantes.SVG.Xamarino.Frente.OLHO_ESQUERDO_FECHADO
+                }
+            };
+        }
+
+        public static OlhosFrames Esquerda()
+        {
+            return new OlhosFrames() {
+                Direito = new OlhoFrame() {
+                    Aberto = Constantes.SVG.Xamarino.Esquerda.OLHO_DIREITO_ABERTO,
+                    Piscando = Constantes.SVG.Xamarino.Esquerda.OLHO_DIREITO_PISCANDO,
+                    Fechado = Constantes.SVG.Xamarino.Esquerda.OLHO_DIREITO_FECHADO
+                },
+                Esquerdo = new OlhoFrame() {
+                    Aberto = Constantes.SVG.Xamarino.Esquerda.OLHO_ESQUERDO_ABERTO,
+                    Piscando = Constantes.SVG.Xamarino.Esquerda.OLHO_ESQUERDO_PISCANDO,
+                    Fechado = Constantes.SVG.Xamarino.Esquerda.OLHO_ESQUERDO_FECHADO
+                }
+            };
+        }
+
+        public static OlhosFrames Direita()
+        {
+            return new OlhosFrames() {
+                Direito = new OlhoFrame() {
+                    Aberto = Constantes.SVG.Xamarino.Direita.OLHO_DIREITO_ABERTO,
+                    Piscando = Constantes.SVG.Xamarino.Direita.OLHO_DIREITO_PISCANDO,
+                    Fechado = Constantes.SVG.Xamarino.Direita.OLHO_DIREITO_FECHADO
+                },
+                Esquerdo = new OlhoFrame() {
+                    Aberto = Constantes.SVG.Xamarino.Direita.OLHO_ESQUERDO_ABERTO,
+                    Piscando = Constantes.SVG.Xamarino.Direita.OLHO_ESQUERDO_PISCANDO,
+                    Fechado = Constantes.SVG.Xamarino.Direita.OLHO_ESQUERDO_FECHADO
+                }
+            };
+        }
+
+        public static OlhosFrames Cima()
+        {
+            return new OlhosFrames() {
+                Direito = new OlhoFrame() {
+                    Aberto = Constantes.SVG.Xamarino.Cima.OLHO_DIREITO_ABERTO,
+                    Piscando = Constantes.SVG.Xamarino.Cima.OLHO_DIREITO_PISCANDO,
+                    Fechado = Constantes.SVG.Xamarino.Cima.OLHO_DIREITO_FECHADO
+                },
+                Esquerdo = new OlhoFrame() {
+                    Aberto = Constantes.SVG.Xamarino.Cima.OLHO_ESQUERDO_ABERTO,
+                    Piscando = Constantes.SVG.Xamarino.Cima.OLHO_ESQUERDO_PISCANDO,
+                    Fechado = Constantes.SVG.Xamarino.Cima.OLHO_ESQUERDO_FECHADO
+                }
+            };
+        }
+
+        public static OlhosFrames Baixo()
+        {
+            return new OlhosFrames() {
+                Direito = new OlhoFrame() {
+                    Aberto = Constantes.SVG.Xamarino.Baixo.OLHO_DIREITO_ABERTO,
+                    Piscando = Constantes.SVG.Xamarino.Baixo.OLHO_DIREITO_PISCANDO,
+                    Fechado = Constantes.SVG.Xamarino.Baixo.OLHO_DIREITO_FECHADO
+                },
+                Esquerdo = new OlhoFrame() {
+                    Aberto = Constantes.SVG.Xamarino.Baixo.OLHO_ESQUERDO_ABERTO,
+                    Piscando = Constantes.SVG.Xamarino.Baixo.OLHO_ESQUERDO_PISCANDO,
+                    Fechado = Constantes.SVG.Xamarino.Baixo.OLHO_ESQUERDO_FECHADO
+                }
+            };
+        }
+    }
+
+    public struct OlhoFrame
+    {
+        public string Aberto { get; set; }
+
+        public string Piscando { get; set; }
+
+        public string Fechado { get; set; }
+
     }
 
     public enum XamarinoPosicao
