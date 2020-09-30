6f59c9e8f372cd1d1c0b2384dd8046d2a3f5efb4
Felipe Baltazar <felipe.dasilvabaltazar@gmail.com>
2020-09-29 22:09:41 -0300

    ✨ Extensions

---
+++ Xamarin.Community.BR/Xamarin.Community.BR/Extensions/SkiaExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SkiaSharp;
+
+namespace Xamarin.Community.BR.Extensions
+{
+    internal static class SkiaExtensions
+    {
+        private const int DEFAULT_XAXISROTATE = 0;
+
+        internal static SKPath ArcTo(this SKPath path, float radius, SKPoint finalPoint)
+        {
+            path.ArcTo(
+                new SKPoint(radius, radius),
+                DEFAULT_XAXISROTATE,
+                SKPathArcSize.Small, SKPathDirection.Clockwise,
+                finalPoint);
+
+            return path;
+        }
+    }
+}
