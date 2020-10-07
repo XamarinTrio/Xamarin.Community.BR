2f738962e04c3d90742aef9d1b52889d6934cf22
Felipe Baltazar <felipe.dasilvabaltazar@gmail.com>
2020-10-06 22:22:18 -0300

    ✨Criado controle de mapa

---
+++ Xamarin.Community.BR/Xamarin.Community.BR/Helpers/ResourcesHelper.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+using System.Reflection;
+
+namespace Xamarin.Community.BR.Helpers
+{
+    public static class ResourcesHelper
+    {
+        private static readonly Assembly _assembly = Assembly.GetExecutingAssembly();
+
+        public static Stream CarregarRecurso(string nome)
+        {
+            return _assembly.GetManifestResourceStream($"Xamarin.Community.BR.Resources.{nome}");
+        }
+    }
+}
