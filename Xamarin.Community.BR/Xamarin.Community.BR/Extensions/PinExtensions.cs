19dc655891b055dd4059e93ba42d2716ea17945a
Felipe Baltazar <felipe.dasilvabaltazar@gmail.com>
2020-10-23 22:54:03 -0300

    ✨ Criada Geolocalizacao

---
+++ Xamarin.Community.BR/Xamarin.Community.BR/Extensions/PinExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xamarin.Community.BR.Abstractions;
+using Xamarin.Community.BR.Helpers;
+using Xamarin.Forms;
+
+namespace Xamarin.Community.BR.Extensions
+{
+    public static class PinExtensions
+    {
+        public static Pin ToPin(this IAmACommunityMember membro)
+        {
+            var avatarUri = new Uri($"{Constantes.Gravatar.URL_BASE}{membro.GravatarHash}");
+            var avatar = ImageSource.FromUri(avatarUri);
+            var localizacao = membro.Localizacao;
+            
+            return new Pin(localizacao.Latitude, localizacao.Longitude, avatar);
+        }
+    }
+}
---
+++ Xamarin.Community.BR/Xamarin.Community.BR/Helpers/Pin.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Xamarin.Community.BR.Abstractions;
+using Xamarin.Forms;
+
+namespace Xamarin.Community.BR.Helpers
+{
+    public struct Pin : IPin, IEquatable<Pin>
+    {
+        private readonly double _latitude;
+        private readonly double _longitute;
+        private readonly ImageSource _avatar;
+
+        public Pin(double latitude, double longitude, ImageSource avatar)
+        {
+            _latitude = latitude;
+            _longitute = longitude;
+            _avatar = avatar;
+        }
+
+        #region IPin
+
+        public ImageSource GetAvatar() => _avatar;
+
+        public double GetLatitude() => _latitude;
+
+        public double GetLongitude() => _longitute;
+
+        #endregion
+
+        #region IEquatable<GeoLocalizacao>
+
+        public bool Equals(Pin other)
+        {
+            return _latitude == other._latitude &&
+                   _longitute == other._longitute &&
+                   _avatar == other._avatar;
+        }
+
+        #endregion
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Pin pin)
+                return Equals(pin);
+
+            return false;
+        } 
+    }
+}
