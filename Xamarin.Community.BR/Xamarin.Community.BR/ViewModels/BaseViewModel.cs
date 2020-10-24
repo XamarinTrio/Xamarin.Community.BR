643d03d5ddfab278e696b7b7bedc7d2b57711ffb
Felipe Baltazar <felipe.dasilvabaltazar@gmail.com>
2020-10-23 22:51:06 -0300

    ✨Criadas viewmodels

---
+++ Xamarin.Community.BR/Xamarin.Community.BR/ViewModels/BaseViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Xamarin.Community.BR.Helpers;
+
+namespace Xamarin.Community.BR.ViewModels
+{
+    public abstract class BaseViewModel : ObservableObject
+    {
+        private bool estaOcupado;
+
+        public bool EstaOcupado
+        {
+            get => estaOcupado;
+            set => SetProperty(ref estaOcupado, value);
+        }
+
+        /// <summary>
+        /// Executa uma tarefa demorada definindo 'EstaOcupado = true' no inicio e 'EstaOcupado = false' ao terminar a tarefa
+        /// </summary>
+        /// <param name="tarefa">Tarefa a ser executada</param>
+        /// <param name="token">Token de cancelamento</param>
+        /// <returns></returns>
+        protected virtual async Task ExecutarTarefa(Func<Task> tarefa, CancellationToken token)
+        {
+            if (EstaOcupado || token.IsCancellationRequested)
+                return;
+
+            EstaOcupado = true;
+
+            try
+            {
+                var taskCompletationSource = new TaskCompletionSource<bool>();
+                using (var registration = token.Register(() => taskCompletationSource.SetCanceled()))
+                {
+                    await Task.WhenAny(tarefa(), taskCompletationSource.Task).ConfigureAwait(false);
+                }
+            }
+            finally
+            {
+                EstaOcupado = false;
+            }
+        }
+    }
+}
