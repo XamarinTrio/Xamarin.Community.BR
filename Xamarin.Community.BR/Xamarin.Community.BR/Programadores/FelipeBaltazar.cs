1df6a1d129aad46d4bb79999ea39ac29a2eb32cc
Felipe Baltazar <felipe.dasilvabaltazar@gmail.com>
2020-10-23 22:50:20 -0300

    Perfil programador

---
+++ Xamarin.Community.BR/Xamarin.Community.BR/Programadores/FelipeBaltazar.cs
@@ -0,0 +1,16 @@
+﻿using Xamarin.Community.BR.Abstractions;
+using Xamarin.Community.BR.Helpers;
+
+namespace Xamarin.Community.BR.Programadores
+{
+    public sealed class FelipeBaltazar : IAmACommunityMember
+    {
+        public string Nome => "Felipe";
+
+        public string SobreNome => "Baltazar";
+
+        public string GravatarHash => "c4deac62305f590fbda80209628afd0e";
+
+        public GeoLocalizacao Localizacao => new GeoLocalizacao(-29.940163, -51.088751);
+    }
+}
