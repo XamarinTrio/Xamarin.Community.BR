25b0ec3415c9eb591fc2d9a8909dadc3daf1f22d
Felipe Baltazar <felipe.dasilvabaltazar@gmail.com>
2020-10-27 22:23:12 -0300

    ✨ Criada DevsPage

---
+++ Xamarin.Community.BR/Xamarin.Community.BR/ViewModels/Paginas/DevsPageViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Xamarin.Community.BR.ViewModels.Paginas
+{
+    public sealed class DevsPageViewModel : BaseViewModel
+    {
+        public ICommand PesquisaCommand { get; }
+
+        private string texto;
+
+        public string Texto
+        {
+            get => texto;
+            set => SetProperty(ref texto, value);
+        }
+
+        public DevsPageViewModel()
+        {
+            PesquisaCommand = new Command(PesquisaCommandExecute);
+        }
+
+        private void PesquisaCommandExecute(object obj)
+        {
+            //Todo
+        }
+    }
+}
